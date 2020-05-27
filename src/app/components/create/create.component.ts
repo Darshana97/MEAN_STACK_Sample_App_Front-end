fc2b294009eaa2d260c1cd77ad965d75aad47e26
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-27 10:38:31 +0530

    create page

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -10,7 +10,28 @@ import { IssueService } from "src/app/issue.service";
   styleUrls: ["./create.component.css"],
 })
 export class CreateComponent implements OnInit {
-  constructor(private issueService: IssueService) {}
+  createForm: FormGroup;
+
+  constructor(
+    private issueService: IssueService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.createForm = this.fb.group({
+      title: ["", Validators.required],
+      responsible: "",
+      description: "",
+      severity: "",
+    });
+  }
+
+  addIssue(title, responsible, description, severity) {
+    this.issueService
+      .addIssue(title, responsible, description, severity)
+      .subscribe(() => {
+        this.router.navigate(["/list"]);
+      });
+  }
 
   ngOnInit(): void {}
 }
