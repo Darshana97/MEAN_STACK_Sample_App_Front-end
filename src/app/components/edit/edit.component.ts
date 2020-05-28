82202bbd09b5ceb13baeb81190b7abc3a9243471
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-05-28 08:34:18 +0530

    import modules

--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from '@angular/router';
 import { IssueService } from "src/app/issue.service";
+
 
 @Component({
   selector: "app-edit",
